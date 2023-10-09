11d756c9c483d8eac8a2e1bba26ec6ee4fa805c5
unknown <ryjwink@gmail.com>
2023-10-09 13:09:25 -0500

    final changes for test

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -2,6 +2,8 @@
 
 const input = require("readline-sync");
 
+
+
 const oldPointStructure = {
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
   2: ['D', 'G'],
@@ -12,16 +14,26 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
+
+
+// your job is to finish writing these functions and variables that we've named //
+// don't change the names or your program won't work as expected. //
+
+function initialPrompt() {
+   console.log("Let's play some scrabble!");
+   word = input.question("\nEnter a word to score: ");
+};
+
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
-	let letterPoints = "";
+	let letterPoints = 0;
  
 	for (let i = 0; i < word.length; i++) {
  
 	  for (const pointValue in oldPointStructure) {
  
 		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+			letterPoints += Number (pointValue)
 		 }
  
 	  }
@@ -29,31 +41,104 @@ function oldScrabbleScorer(word) {
 	return letterPoints;
  }
 
-// your job is to finish writing these functions and variables that we've named //
-// don't change the names or your program won't work as expected. //
+function simpleScorer (word){
+   word = word.toUpperCase();
+   let simplePoints = 0;
 
-function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   for (i = 0; i < word.length; i++){
+     Number (simplePoints ++)
+   }
+   return simplePoints
 };
 
-let simpleScorer;
 
-let vowelBonusScorer;
+function vowelBonusScorer(word){
+word = word.toUpperCase();
+      let bonusVowelScore = 0;
+            const vowels = ["A", "E", "I", "O", "U"];
+         for (let i = 0; i < word.length; i++) {
+             if (vowels.includes(word[i])) {
+                  bonusVowelScore += 3;
+             } else {
+                  bonusVowelScore += 1;
+               }
+         }
+         return bonusVowelScore
 
-let scrabbleScorer;
 
-const scoringAlgorithms = [];
+};
 
-function scorerPrompt() {}
 
-function transform() {};
+let scrabble = {
+   name: "Scrabble",
+   description:"The traditional scoring algorithm.",
+   scorerFunction:  scrabbleScorer
+};
 
-let newPointStructure;
+let bonusVowelScorer = {
+   name: "Bonus vowels",
+   description:"Vowels are 3 pts, consonants are 1 pt.",
+   scorerFunction:  vowelBonusScorer
+       
+};
+
+let simpleScore = {
+   name: "Simple Score",
+   description:"Each letter is worth 1 point",
+   scorerFunction:  simpleScorer
+};
+
+
+function transform(oldPointStructure) {
+   let transformed = {}
+   for (key in oldPointStructure) {
+      for (i = 0; i < oldPointStructure[key].length; i++){
+         transformed[oldPointStructure[key][i].toLowerCase()] = Number (key);
+      }
+   }
+   return transformed
+
+};
+
+let newPointStructure = transform(oldPointStructure);
+
+function scrabbleScorer (word){
+   word = word.toLowerCase();
+   let scrabbleScore = 0;
+   for (i = 0; i < word.length; i ++){
+     if (newPointStructure.hasOwnProperty(word[i]))
+     scrabbleScore += newPointStructure[word[i]]
+   }
+   return scrabbleScore; 
+
+}
+
+
+const scoringAlgorithms = [simpleScore,bonusVowelScorer,scrabble];
+
+
+function scorerPrompt() {
+let chosenScoreStyle = input.question("Which scoring method would you like to use?\n\n 0 - Simple: One point per character\n 1 - Vowel Bonus: Vowels are worth 3 points\n 2 - Scrabble: Uses scrabble point system\n Enter 0, 1, or 2: ")
+if (chosenScoreStyle === "0"){
+      return console.log (`Score for ${word}: ${scoringAlgorithms[0].scorerFunction(word)}`)
+   }else if (chosenScoreStyle === "1"){
+         return console.log (`Score for '${word}': ${scoringAlgorithms[1].scorerFunction(word)}`)
+      } else if (chosenScoreStyle === "2") {
+            return console.log(`Score for '${word}': ${scoringAlgorithms[2].scorerFunction(word)}` )
+            } 
+   
+};
+
+
 
 function runProgram() {
    initialPrompt();
+   scorerPrompt();
+
+   
    
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
